6857ecf41bf4bdebced6a8503ea49f0a15147a44
PieroJacinto <pierojacinto@gmail.com>
2025-02-13 14:35:34 -0300

    vercel 3

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
-import { createApp } from '../server/src/index.js';
+import { createApp } from './src/index.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
