4bbbfe733d6f7c6013f63b495e9e94f82ac0dc1f
PieroJacinto <pierojacinto@gmail.com>
2025-02-13 21:55:05 -0300

    vercel changes 5

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,17 +1,24 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import * as process from 'process';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
   build: {
     outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: true,
+    emptyOutDir: true,
   },
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: true
+        target: process.env.VITE_API_URL || 'http://localhost:3000',
+        changeOrigin: true,
+        secure: false,
       }
     }
   }
-});
+})
